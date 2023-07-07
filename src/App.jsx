aad551e366f19f88a6a1bd56dcf8131bbd2fca9d
lucre <lucreciagarino@gmail.com>
2023-07-07 12:54:08 +0200

    layout fixes, theme config

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Flex } from "@chakra-ui/react";
 import Projects from "./pages/Projects";
 import Gallery from "./pages/Gallery";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-    return (
-        <>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Projects />} />
-                <Route path="/gallery" element={<Gallery />} />
-                <Route path="/about" element={<About />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <Flex direction='column' grow={1}>
+        <Routes>
+          <Route path='/' element={<Projects />} />
+          <Route path='/gallery' element={<Gallery />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Flex>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,38 @@ import "./index.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 // set custom fonts, colors, etc.
-const theme = extendTheme({});
+const theme = extendTheme({
+  textStyles: {
+    h2: {
+      // array for sm & bg values
+      fontSize: ["2rem", "2.25rem"],
+      lineHeight: ["2.5rem", "3rem"],
+      fontWeight: "500",
+    },
+    h3: {
+      fontSize: ["1.5rem", "1.75rem"],
+      lineHeight: ["2.25rem", "2.5rem"],
+      fontWeight: "500",
+    },
+    p: {
+      fontSize: ["1.25rem", "1.5rem"],
+      lineHeight: ["2.25rem", "2rem"],
+      fontWeight: "500",
+    },
+    a: {
+      fontSize: ["1rem", "1.12rem"],
+      lineHeight: ["1.75rem", "1.75rem"],
+      fontWeight: "500",
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <ChakraProvider theme={theme}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </ChakraProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
 );
