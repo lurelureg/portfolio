daa5803538ce2bf7bc3be556fc578e7dab609704
lucre <lucreciagarino@gmail.com>
2023-07-07 12:54:56 +0200

    Navbar, footer, and gallery updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,11 @@
-export default function Footer() {
-    return <div>Footer</div>;
+import { Flex, Text, Heading } from "@chakra-ui/react";
 
-    <footer>
-        <div class="contact" id="contact">
-            <h3>Contact</h3>
-            </hr>
-        </div>
-        <nav class="footer" id="contact">
-            <div class="left">
-                <a href="" type="email">Send me an email <span>&#8599;</span></a>
-                <a href="#top">Back to top &#8593;</a>
-            </div>
-            <div class="right">
-                <a href="" target="_blank">LinkedIn &#8599;</a>
-                <a href="" target="_blank">Instagram &#8599;</a>
-                <a href="" target="_blank">Dribbble &#8599;</a>
-                <a href="" target="_blank">Behance &#8599;</a>
-            </div>
-        </nav>
-    </footer>
+export default function Footer() {
+  return (
+    <Flex bg='blackAlpha.800' h={100} mx='-2rem' mt='4rem' padding='0 2rem'>
+      <Text textStyle='h2' as='h2'>
+        Visit the Gallery for more Visual work, and please
+      </Text>
+    </Flex>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,35 @@
 import lucreLogo from "/logoLucre.svg";
 import { Link, NavLink } from "react-router-dom";
+import { Flex, Text } from "@chakra-ui/react";
 
 export default function Navbar() {
-    return (
-        <nav className="navbar">
-            <Link to="/">
-                <img src={lucreLogo} className="logoimg" alt="Lucre logo" />
-            </Link>
-            <div className="linksNav">
-                <NavLink className="link" to="/">
-                    Projects
-                </NavLink>
-                <NavLink className="link" to="/gallery">
-                    Gallery
-                </NavLink>
-                <NavLink className="link" to="/about">
-                    About
-                </NavLink>
-            </div>
-        </nav>
-    );
+  return (
+    <Flex
+      justifyContent='space-between'
+      h={135}
+      direction={{ base: "column", sm: "row" }}
+      alignItems={{ base: "center", sm: "unset" }}
+    >
+      <Link to='/'>
+        <img src={lucreLogo} className='logoimg' alt='Lucre logo' />
+      </Link>
+      <div>
+        <NavLink className='link' to='/'>
+          <Text textStyle='a' as='span'>
+            Projects
+          </Text>
+        </NavLink>
+        <NavLink className='link' to='/gallery'>
+          <Text textStyle='a' as='span'>
+            Gallery
+          </Text>
+        </NavLink>
+        <NavLink className='link' to='/about'>
+          <Text textStyle='a' as='span'>
+            About
+          </Text>
+        </NavLink>
+      </div>
+    </Flex>
+  );
 }
