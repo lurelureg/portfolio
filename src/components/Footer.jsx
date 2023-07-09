e80b309c42be6c343c9ace1e48cfc504814fd88c
lucre <lucreciagarino@gmail.com>
2023-07-10 01:02:46 +0200

    various style fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,99 +3,97 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import IMAGES from "../images/images";
 
 export default function Footer() {
-    const toast = useToast();
+  const toast = useToast();
 
-    return (
-        <>
-            <Flex
-                as="footer"
-                justifyContent="space-between"
-                direction={{ base: "column", sm: "row-reverse" }}
-                alignItems="center"
-                padding="2rem 0"
-                gap={{ base: "2rem" }}
+  return (
+    <>
+      <Flex
+        as='footer'
+        justifyContent='space-between'
+        direction={{ base: "column", sm: "row-reverse" }}
+        alignItems='center'
+        padding='2rem 0'
+        gap={{ base: "2rem" }}
+      >
+        {/* Socials */}
+        <Flex gap={"0.75rem"}>
+          <CopyToClipboard text={"lucreciagarino@gmail.com"}>
+            <Link
+              onClick={() =>
+                toast({
+                  title: "Copied to clipboard 📋",
+                  duration: 2000,
+                  status: "success",
+                })
+              }
             >
-                {/* Socials */}
-                <Flex gap={"0.75rem"}>
-                    <CopyToClipboard text={"lucreciagarino@gmail.com"}>
-                        <Link
-                            onClick={() =>
-                                toast({
-                                    title: "Copied to clipboard 📋",
-                                    duration: 2000,
-                                    status: "success",
-                                })
-                            }
-                        >
-                            <Image
-                                src={IMAGES.socials.email}
-                                className="socialIcon"
-                                alt="email me"
-                            />
-                        </Link>
-                    </CopyToClipboard>
+              <Image
+                src={IMAGES.socials.email}
+                className='socialIcon'
+                alt='email me'
+              />
+            </Link>
+          </CopyToClipboard>
 
-                    <Link
-                        href="https://www.linkedin.com/in/lucreciagarino"
-                        isExternal
-                        color="cyan.200"
-                    >
-                        <Image
-                            src={IMAGES.socials.linkedin}
-                            className="socialIcon"
-                            alt="link to LinkedIn"
-                        />
-                    </Link>
-                    <Link
-                        href="https://www.behance.net/presenteperfecto"
-                        isExternal
-                        color="cyan.200"
-                    >
-                        <Image
-                            src={IMAGES.socials.behance}
-                            className="socialIcon"
-                            alt="link to Behance"
-                        />
-                    </Link>
-                    <Link
-                        href="https://instagram.com/collage.toast"
-                        isExternal
-                        color="cyan.200"
-                    >
-                        <Image
-                            src={IMAGES.socials.instagram}
-                            className="socialIcon"
-                            alt="link to Instagram, collage account"
-                        />
-                    </Link>
-                    <Link
-                        href="https://dribbble.com/presenteperfecto"
-                        isExternal
-                        color="cyan.200"
-                    >
-                        <Image
-                            src={IMAGES.socials.dribbble}
-                            className="socialIcon"
-                            alt="link to Dribbble"
-                        />
-                    </Link>
-                    <Link
-                        href="https://github.com/lurelureg"
-                        isExternal
-                        color="cyan.200"
-                    >
-                        <Image
-                            src={IMAGES.socials.github}
-                            className="socialIcon"
-                            alt="link to Github"
-                        />
-                    </Link>
-                </Flex>
+          <Link
+            href='https://www.linkedin.com/in/lucreciagarino'
+            isExternal
+            color='cyan.200'
+          >
+            <Image
+              src={IMAGES.socials.linkedin}
+              className='socialIcon'
+              alt='link to LinkedIn'
+            />
+          </Link>
+          <Link
+            href='https://www.behance.net/presenteperfecto'
+            isExternal
+            color='cyan.200'
+          >
+            <Image
+              src={IMAGES.socials.behance}
+              className='socialIcon'
+              alt='link to Behance'
+            />
+          </Link>
+          <Link
+            href='https://instagram.com/collage.toast'
+            isExternal
+            color='cyan.200'
+          >
+            <Image
+              src={IMAGES.socials.instagram}
+              className='socialIcon'
+              alt='link to Instagram, collage account'
+            />
+          </Link>
+          <Link
+            href='https://dribbble.com/presenteperfecto'
+            isExternal
+            color='cyan.200'
+          >
+            <Image
+              src={IMAGES.socials.dribbble}
+              className='socialIcon'
+              alt='link to Dribbble'
+            />
+          </Link>
+          <Link href='https://github.com/lurelureg' isExternal color='cyan.200'>
+            <Image
+              src={IMAGES.socials.github}
+              className='socialIcon'
+              alt='link to Github'
+            />
+          </Link>
+        </Flex>
 
-                <Text textStyle="small" as="p" m={0}>
-                    Copyright © Lucrecia Garino 2023. All rights reserved.
-                </Text>
-            </Flex>
-        </>
-    );
+        <Link href='#top'>Back to top ↑</Link>
+
+        <Text textStyle='small' as='p' m={0}>
+          Copyright © Lucrecia Garino 2023. All rights reserved.
+        </Text>
+      </Flex>
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -9,55 +9,57 @@ import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
 
 const theme = extendTheme({
-    fonts: {
-        body: "Poppins, system-ui, sans-serif",
+  fonts: {
+    body: "Poppins, system-ui, sans-serif",
+  },
+  styles: {
+    global: {
+      body: {
+        backgroundColor: "#F5F5F5",
+        color: "black",
+        p: ["0 1rem", "0 2rem"],
+        m: ["0 auto", "0 auto"],
+        maxWidth: "85em",
+      },
     },
-    styles: {
-        global: {
-            body: {
-                backgroundColor: "#F5F5F5",
-                color: "black",
-                m: ["0 1rem", "0 2rem"],
-            },
-        },
+  },
+  textStyles: {
+    h2: {
+      fontSize: ["1.75rem", "2rem"],
+      lineHeight: ["2.25rem", "2.5rem"],
+      fontWeight: "500",
     },
-    textStyles: {
-        h2: {
-            fontSize: ["1.75rem", "2rem"],
-            lineHeight: ["2.25rem", "2.5rem"],
-            fontWeight: "500",
-        },
-        h3: {
-            fontSize: ["1.5rem", "1.75rem"],
-            lineHeight: ["2.25rem", "2.5rem"],
-            fontWeight: "500",
-        },
-        p: {
-            fontSize: ["1.25rem", "1.40rem"],
-            lineHeight: ["1.75rem", "2rem"],
-            fontWeight: "400",
-            color: "blackAlpha.800",
-        },
-        a: {
-            fontSize: ["1rem", "1.12rem"],
-            lineHeight: ["1.75rem", "1.75rem"],
-            fontWeight: "500",
-        },
+    h3: {
+      fontSize: ["1.5rem", "1.75rem"],
+      lineHeight: ["2.25rem", "2.5rem"],
+      fontWeight: "500",
+    },
+    p: {
+      fontSize: ["1.25rem", "1.40rem"],
+      lineHeight: ["1.75rem", "2rem"],
+      fontWeight: "400",
+      color: "blackAlpha.800",
+    },
+    a: {
+      fontSize: ["1rem", "1.12rem"],
+      lineHeight: ["1.75rem", "1.75rem"],
+      fontWeight: "500",
+    },
 
-        small: {
-            fontSize: ["0.75rem"],
-            lineHeight: ["1rem", "1rem"],
-            fontWeight: "500",
-        },
+    small: {
+      fontSize: ["0.75rem"],
+      lineHeight: ["1rem", "1rem"],
+      fontWeight: "500",
     },
+  },
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <ChakraProvider theme={theme}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </ChakraProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
 );
