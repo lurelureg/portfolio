2157fd4a617a8d402689a381d9ad01b40fdad8d6
lurelureg <lucreciagarino@gmail.com>
2024-06-02 13:17:43 +0200

    about change

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,61 +4,32 @@ import PageWrapper from "../components/PageWrapper";
 export default function About() {
     return (
         <PageWrapper>
+            
+            <Text color={"#1b1b1bc9"}
+                textStyle="h3"
+                as="h3"
+                w={{ base: "unset", md: "80%", lg: "65%" }}
+            >
+                <small>
+                    Hola! Hi! Salut! Ciao! Moin! Ahoy! Namaste! <br />
+                    </small>
+
+            </Text>
             <Text
                 textStyle="h2"
                 as="h2"
                 w={{ base: "unset", md: "80%", lg: "65%" }}
             >
-                Lucrecia Garino is a multifaceted designer with a focus on
-                Branding, Creative Strategy, and Digital Mediums.
+                I&apos;m Lucrecia, a designer based in Berlin. <br />I transform businesses with strategic branding and engaging visuals, blending creativity and optimization.
             </Text>
-            <Text
-                textStyle="h3"
-                as="h3"
-                w={{ base: "unset", md: "80%", lg: "65%" }}
-            >
-                <small>Currently based in Berlin. 🐻</small>
-            </Text>
+
             <Text
                 textStyle="p"
                 as="p"
                 w={{ base: "unset", md: "75%", lg: "65%" }}
             >
-                As a seasoned Brand Strategist and Communication Designer since
-                2010, I&apos;ve been at the forefront of crafting compelling
-                brand identities and designing impactful visual communication
-                strategies. My expertise spans diverse industries, including
-                environmental nonprofits, healthcare, wellness, virtual events,
-                and civic organizations. I specialize in bridging business
-                values and objectives into compelling discourses that resonate
-                with target audiences.
-                <br />
-                My educational background includes a Diploma in Graphic &
-                Advertising Design, a Certificate Program in{" "}
-                <strong>Creativity for Advertising</strong> . I further pursued
-                a BA in Human-Centered Design.
-                <br /> <br />
-                I enjoyed complementing my experience teaching and mentoring in
-                Graphic Design, I led the development of courses for
-                professionals, emphasizing the creation of complete design
-                projects. Guiding students, I provided hands-on leadership,
-                ensuring successful outcomes and contributing to their final
-                degree achievements.
-                <br />
-                <br />I recently completed a Bootcamp in <strong>UX/UI</strong>,
-                and another in Web <strong>Web Development</strong>. I consider
-                a continuous pursuit of education is key to staying ahead in the
-                ever-changing landscape of design and technology.
-                <br />
-                When I&apos;m not studying or collaging, you&apos;ll find me
-                exploring the infinite city of Berlin, hunting for stucco
-                sculptures and unusual plants.
-                <br />
-                <br /> I&apos;m currently on the lookout for an amazing team to
-                join. I thrive in multidisciplinary environments, where I can
-                empower others and create something beyond great together.
-                I&apos;m excited to take on new challenges and make a real
-                impact. Let&apos;s make things happen! <br />
+With over 10 years as a visual designer, I&apos;ve honed my craft across sectors like retail, banking, wholesale, healthcare, and civic organizations. <br />I specialize in re-branding, leading brand workshops, and <strong>bridging business goals with creativity.</strong> My approach results in companies that are proud and eager to share their work.<br /><br /> Born into a creative family, I&apos;m curious and proactive. My background includes Graphic & Advertising Design and Human-Centered Design. I&apos;ve expanded my expertise through certifications in <strong> creativity, UX/UI, and Web Development </strong> Bootcamps. Continuous learning is fueled by my passion for multidisciplinary collaboration.<br /><br />Beyond the screen I&apos;m an avid explorer of Berlin, you&apos;ll find me taking pictures of its flora and architecture, lost in art galleries and museums or with my concert buddies. <br /><br />I&apos;m always seeking new challenges for new challenges or opportunities to collaborate, so please get in touch, even for a short chat.
+<br /><strong>Let&apos;s make things happen!</strong>
             </Text>
 
             <Flex
@@ -71,48 +42,36 @@ export default function About() {
                 <Box flex="1 0 auto">
                     <Text>
                         <strong>Soft Skills</strong> <br />
-                        Leadership <br /> Communication <br /> Collaboration
-                        <br />
-                        Creativity
-                        <br /> Adaptability
-                        <br /> Mentoring <br />
-                        Workshop Facilitation
-                        <br /> Client-Facing Communication <br /> Team
-                        Empowerment <br /> Problem-Solving
+                        Leadership<br />
+                        Communication<br />
+                        Collaboration<br />
+                        Creativity<br />
+                        Adaptability<br />
+                        Mentoring<br />
+                        Problem-Solving
                     </Text>
                 </Box>
                 <Box flex="1 0 auto">
                     <Text>
                         <strong>Hard Skills</strong> <br />
-                        Brand Strategy
-                        <br />
-                        Communication Design
-                        <br />
-                        Visual Design
-                        <br />
-                        Creative Strategy
-                        <br />
-                        Conversion Optimization
-                        <br />
-                        Information Architecture
-                        <br />
-                        Campaign Development
-                        <br />
-                        Creative Conceptualization
-                        <br />
-                        Web Development
-                        <br />
+                        Brand Strategy<br />
+                        Communication Design<br />
+                        Visual Design<br />
+                        Creative Strategy<br />
+                        Conversion Optimization<br />
+                        Information Architecture<br />
+                        Web Development<br />
                         UX/UI Design
-                        <br />
-                        Creative Content
-                        <br />
                     </Text>
                 </Box>
                 <Box flex="1 0 auto">
                     <Text>
                         <strong>Favorite Tools</strong> <br />
-                        Figma <br />
-                        Adobe <br /> Jira <br /> Notion <br /> Google
+                        Figma<br />
+                        Adobe Suite<br />
+                        Jira<br />
+                        Notion<br />
+                        Google One Drive
                     </Text>
                 </Box>
             </Flex>
